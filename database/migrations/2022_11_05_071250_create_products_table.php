b5419db06ec10ab724f2a9a2bf0a4517bcbfd86e
Bzhyks <paulius.voveraitis@protonmail.com>
2022-11-05 10:00:11 +0200

    Initial commit

---
+++ database/migrations/2022_11_05_071250_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 64);
+            $table->integer('quantity');
+            $table->foreignId('warehouse_id')->constrained();
+            // $table->unsignedBigInteger('warehouse_id');
+            $table->timestamps();
+            // $table->foreign('warehouse_id')->references('id')->on('warehouses');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
